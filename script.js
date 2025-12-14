d0b3861cd64b31d6b21b978b76e05532994dc6e7
Cidragon <darioperezpastrian@gmail.com>
2025-12-14 00:51:01 -0300

    add start and end point

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
 
-const columns = 20
-const rows = 20
+const BACKGROUND_COLOR = 'rgb(250,0,250)'
+const START_COLOR = 'rgba(0, 255, 0, 1)'
+const END_COLOR = 'rgba(0,0,255,1)'
 
-const cells = [];
+let columns = 20
+let rows = 20
+let start_points = [0,0]
+let end_points = [0,0]
 
 function create_grid(){
-    const container = document.getElementById('grid-container');
-    console.log(container);
+    const container = document.getElementById('grid-container')
+
     for (let y = 0; y < rows; y++) {
         const row = document.createElement('div');
         row.classList.add('row')
         for( let x = 0; x < columns; x++){
             const cell = document.createElement('div');
-
-            let R = Math.floor(Math.random() * 256)
-            let G = Math.floor(Math.random() * 256)
-            let B = Math.floor(Math.random() * 256)
-
-            //cell.style.backgroundColor = `rgb(${R},${G},${B})`
-            //cell.innerHTML = x.toString() + "," + y.toString()
-            //console.log(cell.value)
             cell.classList.add('cell');
+            let id = x.toString() + " " + y.toString()
+            cell.id = id
+            
+            if (start_points[0] == x && start_points[1] == y){
+                cell.style.backgroundColor = START_COLOR
+            }
+
+            if (end_points[0] == x && end_points[1] == y){
+                cell.style.backgroundColor = END_COLOR
+            }
             row.appendChild(cell);
-            //container.appendChild(cell)
         }
         container.appendChild(row)
     }
     return ""
 }
 
+function generate_start_and_end_points(){
+    start_points[0] = Math.floor(Math.random() * columns)
+    start_points[1] = Math.floor(Math.random() * rows)
+
+    end_points[0] = Math.floor(Math.random() * columns)
+    end_points[1] = Math.floor(Math.random() * rows)
+
+    while (start_points[0] == end_points[0] && start_points[1] == end_points[1]){
+        end_points[0] = Math.floor(Math.random() * columns)
+        end_points[1] = Math.floor(Math.random() * rows)
+    }
+}
+
+generate_start_and_end_points()
 create_grid()
