cc0cbb3c6dd26eb0f3e294e88a6fd34c13793d1a
Cidragon <darioperezpastrian@gmail.com>
2025-12-14 17:50:37 -0300

    drag and drop start and end point

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 
+const SEPARATOR = ","
 const BACKGROUND_COLOR = 'rgb(250,0,250)'
-const START_COLOR = 'rgba(0, 255, 0, 1)'
-const END_COLOR = 'rgba(0,0,255,1)'
+const START_COLOR = 'rgb(0, 255, 0)'
+const END_COLOR = 'rgb(0,0,255)'
+const WALL_COLOR = 'rgb(0,0,0)'
+
+const LEFT_CLICK = 0
+let is_left_hold = false
+let is_start_dragged = false
+let is_end_dragged = false
+let dragged_cell = null
 
 let columns = 20
 let rows = 20
 let start_points = [0,0]
 let end_points = [0,0]
+let drag_start_point = [0,0]
 
 function create_grid(){
     const container = document.getElementById('grid-container')
@@ -17,9 +26,11 @@ function create_grid(){
         for( let x = 0; x < columns; x++){
             const cell = document.createElement('div');
             cell.classList.add('cell');
-            let id = x.toString() + " " + y.toString()
+            let id = x.toString() + SEPARATOR + y.toString()
             cell.id = id
-            
+            cell.addEventListener('dragstart', (e) => e.preventDefault()); //prevent drag and drop
+            cell.addEventListener('mousedown', mouse_pressed_in_grid)
+
             if (start_points[0] == x && start_points[1] == y){
                 cell.style.backgroundColor = START_COLOR
             }
@@ -31,7 +42,6 @@ function create_grid(){
         }
         container.appendChild(row)
     }
-    return ""
 }
 
 function generate_start_and_end_points(){
@@ -41,11 +51,68 @@ function generate_start_and_end_points(){
     end_points[0] = Math.floor(Math.random() * columns)
     end_points[1] = Math.floor(Math.random() * rows)
 
+    if(columns == 1 && rows == 1){
+        console.log("impossible to create start and end point when grid is 1x1")
+        return
+    }
+
     while (start_points[0] == end_points[0] && start_points[1] == end_points[1]){
         end_points[0] = Math.floor(Math.random() * columns)
         end_points[1] = Math.floor(Math.random() * rows)
     }
 }
 
+function mouse_pressed_in_grid(event){
+    if(event.button == LEFT_CLICK){
+        let coordinates = event.target.id.split(SEPARATOR)
+        let x = parseInt(coordinates[0])
+        let y = parseInt(coordinates[1])
+        
+        dragged_cell = event.target
+        if(start_points[0] == x && start_points[1] == y){
+            is_start_dragged = true
+            is_end_dragged = false
+        }
+        else if(end_points[0] == x && end_points[1] == y){
+            is_start_dragged = false
+            is_end_dragged = true
+        }
+        is_left_hold = true
+        console.log("left click pressed in ", is_start_dragged, is_end_dragged)
+    }
+}
+
+function mouse_released_in_grid(event){
+    if(event.button == LEFT_CLICK){
+        let coordinates = event.target.id.split(SEPARATOR)
+        let x = parseInt(coordinates[0])
+        let y = parseInt(coordinates[1])
+
+        if(event.target.classList != "cell"){
+            return console.log("target isn't a cell")
+        }
+
+        if(is_start_dragged){
+            is_start_dragged = false
+            start_points = [x,y]
+            dragged_cell.style.backgroundColor = ''
+            event.target.style.backgroundColor = START_COLOR
+        }else if(is_end_dragged){
+            end_points = [x,y]
+            dragged_cell.style.backgroundColor = ''
+            event.target.style.backgroundColor = END_COLOR
+        }
+        
+        console.log(event.target.id)
+        is_left_hold = false
+        dragged_cell = null
+    }
+}
+
+window.addEventListener('mouseup', (event) => {
+    if (is_left_hold) {
+        mouse_released_in_grid(event);
+    }
+});
 generate_start_and_end_points()
 create_grid()
